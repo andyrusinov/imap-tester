cf50d11465665381698f0ac78b319de163886063
andyrusinov <54044893+andyrusinov@users.noreply.github.com>
2019-08-23 12:55:58 -0700

    Now we process all folders, not just Inbox. Also added "Processing folder" messages

--- test-imap.py
+++ test-imap.py
@@ -3,41 +3,15 @@ import os, urllib.parse, email, ssl
 from imapclient import IMAPClient
 
 
-def main():
-  username,password,servername = os.environ.get('username'), \
-                                 os.environ.get('password'), \
-                                 os.environ.get('server')
+# returns True if one of args is None
+def var_is_none(*args):
+  for h in args:
+    if h is None:
+      return True
+  return False
 
-  ssl_context = ssl.create_default_context()
-  if not os.environ.get('insecure') is None: # variable "insecure" is set, need to disable cert check
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
 
-  # check variables
-  if username is None or \
-    password is None or \
-    servername is None:
-    print('ERROR: need to set username password and server vars:\n' + \
-          'docker run --env username=\'account\' --env password=\'password\'' + \
-          '--env server=\'server\' [--env insecure=yes] --rm -it andyrusinov/imap-tester\n' + \
-	  'optional var [insecure] drops SSL secure requirements\n' + \
-          'Note: this script never saves the messages. All it does is downloading ' + \
-          'them from INBOX, printing their size and dropping them (thus checking the connection)')
-    exit()
-  
-  # connect
-  with IMAPClient(servername, ssl_context=ssl_context) as server:
-    server.login(username, password)
-    server.select_folder('INBOX', readonly=True)
-
-    # find messages, print their size
-    messages = server.search()
-    output = [("uid","From","Subject","Size")]
-    for uid, message_data in server.fetch(messages, 'RFC822').items():
-      email_message = email.message_from_bytes(message_data[b'RFC822'])
-      output += [(uid, email_message.get('From'), email_message.get('Subject'),
-      str(len(email_message.__bytes__() if email_message.__bytes__() else '')))]
-
+def print_tabbed(output):
   # determine string lengths to make output more fancy
   lengths=[0]*4
   format=""
@@ -49,7 +23,53 @@ def main():
     format+='{:'+str(lengths[i])+'}\t'
   for x in output:
     print(format.format(*x))
+  return True
 
+
+# process folder, print number of messages
+def process_folder(server, folder):
+  server.select_folder(folder, readonly=True)
+  messages = server.search()
+  print(f"Processing folder {folder}. {len(messages)} messages found")
+  output = [("uid","From","Subject","Size")] if len(messages) else [] # if there is 0 messages, we are not printing anything
+  for uid, message_data in server.fetch(messages, 'RFC822').items():
+    email_message = email.message_from_bytes(message_data[b'RFC822'])
+    output += [(uid, email_message.get('From'), email_message.get('Subject'),
+    str(len(email_message.__bytes__() if email_message.__bytes__() else '')))]
+  print_tabbed(output)
+  print()
+  return True
+
+  
+
+def main():
+  username,password,servername = os.environ.get('username'), \
+                                 os.environ.get('password'), \
+                                 os.environ.get('server')
+
+  ssl_context = ssl.create_default_context()
+  if not os.environ.get('insecure') is None: # variable "insecure" is set, need to disable cert check
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
+  # check variables
+  if var_is_none(username, password, servername):
+    print('ERROR: need to set username password and server vars:\n' + \
+          'docker run --env username=\'account\' --env password=\'password\'' + \
+          '--env server=\'server\' [--env insecure=yes] --rm -it andyrusinov/imap-tester\n' + \
+	  'optional var [insecure] drops SSL secure requirements\n' + \
+          'Note: this script never saves the messages. All it does is downloading ' + \
+          'them from INBOX, printing their size and dropping them (thus checking the connection)')
+    exit()
+  
+  # connect
+  with IMAPClient(servername, ssl_context=ssl_context) as server:
+    server.login(username, password)
+    folders = [f[-1] for f in server.list_sub_folders()]
+    for folder in folders:
+      process_folder(server, folder)
+
+  print ("Logging out")
 
 
 
